6a286526eb0d825c5e5905a86bd48f3825b6d1c0
Mete Ergin <mete.ergin@outlook.com>
2022-10-22 20:35:12 +0300

    Navbar added.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Navbar from "./component/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+      <Navbar header="Navbar Test" />
+      <App />
+    </>
   </React.StrictMode>
 );
